d7b20b91ef0dd7d16bad104426016d3da62c76f4
joaovictorc92 <joaovictorc92@gmail.com>
2016-09-01 23:19:10 -0300

    construindo builder

--- src/br/com/padroesprojeto/builder/NotaFiscalBuilder.java
+++ src/br/com/padroesprojeto/builder/NotaFiscalBuilder.java
@@ -1,5 +1,31 @@
 package br.com.padroesprojeto.builder;
 
-public class NotaFiscalBuilder {
+import br.com.padroesprojeto.builder.factory.notafiscal.NotaFiscalPessoaFisica;
+import br.com.padroesprojeto.builder.factory.notafiscal.NotaFiscalPessoaJuridica;
 
+public class NotaFiscalBuilder {
+	private NotaFiscal instancia;
+	
+	public NotaFiscalBuilder notaFiscalPessoaFisica(){
+		instancia = new NotaFiscalPessoaFisica();
+		return this;
+	}
+	
+	public NotaFiscalBuilder notaFiscalPessoaJuridica(){
+		instancia = new NotaFiscalPessoaJuridica();
+		return this;
+	}
+	
+	public NotaFiscalBuilder definirProduto(){
+		return this;
+	}
+	
+	public NotaFiscalBuilder definirNumero(int numero){
+		instancia.setNumero(numero);
+		return this;
+	}
+	
+	public NotaFiscalBuilder definirDataEmissao(String dataEmissao){
+		return this;
+	}
 }
